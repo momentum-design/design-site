007bc92e0ba4ea96a0d087fa5d7586ae8029d220
ArthusLiang <ArthusLiang@users.noreply.github.com>
2024-04-24 00:22:05 +0000

    [skip ci](build static site):Merge pull request #3 from ArthusLiang/beta
    feat(beta): create

--- src/lib/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './con';
-export * from './footer';
-export * from './header';
-export * from './nav';
-export * from './nav-item';
-export * from './tab';
-export * from './tools';
--- src/lib/nav-item/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './nav-item.component';
-export * from './nav-item.module';
